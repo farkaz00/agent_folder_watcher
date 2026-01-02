ce4b97e57b3b99326afffcdd788a3e905e913955
Ralf <farkaz00@hotmail.com>
2026-01-02 17:16:58 -0500

    Added procedure to walk the suppplied system path

--- agent_folder_watcher/main.py
+++ agent_folder_watcher/main.py
@@ -1,11 +1,12 @@
-import sys
 from pathlib import Path
 from argparse import ArgumentParser
 
 
 class AgentParams:
     """
-    Holds all parameters required for the Path Watcher Agent's tasks
+    Holds all parameters required for the Path Watcher Agent's tasks.
+
+    - path: path to the resource to watch over
     """
 
     def setPath(self, Path: str):
@@ -22,20 +23,71 @@ def main() -> None:
     params = process_arguments()
     try:
         params.validate()
+        watch_path(params.path)
     except Exception as e:
         print(e)
 
 
 def process_arguments() -> AgentParams:
-    # Add arguments
+    """
+    Maps the input arguments to an AgentParams object
+    """
+    ## Add arguments
     ap = ArgumentParser()
-    ap.add_argument("--path", help="an existing path to use")
+
+    # path: path to the resource to watch
+    ap.add_argument("-p", "--path", required=True, help="an existing path to use")
 
     # Process arguments
     args = ap.parse_args()
     params = AgentParams()
     params.setPath(args.path)
     return params
+
+
+def watch_path(path: str) -> None:
+    """
+    Controls the watcher's main loop
+    """
+    p = Path(path)
+    # while True:
+    # Iterate over the path if it's a directory
+    for root, dirnames, filenames in p.walk():
+        print(f"ROOT: {root}")
+        print(f"DIRNAMES: {dirnames}")
+        print(f"FILENAMES: {filenames}")
+    """
+    for child in p.iterdir():
+        if child.is_dir():
+            watch_dir(child)
+        elif child.is_file():
+            watch_file(child)
+    """
+
+
+def watch_dir(dir: Path) -> None:
+    """
+    Iterates over the input directory
+    """
+    print(f"PATH: {dir.name}")
+    if not dir.is_dir():
+        raise Exception("the input path is not a directory")
+
+    for child in dir.iterdir():
+        if child.is_dir():
+            watch_dir(child)
+        elif child.is_file():
+            watch_file(child)
+
+
+def watch_file(f: Path) -> None:
+    """
+    Processes the input file
+    """
+    if not f.is_file():
+        raise Exception("the input path is not a file")
+
+    print(f"FILE:{f.name}")
 
 
 if __name__ == "__main__":
